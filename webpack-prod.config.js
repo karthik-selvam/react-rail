2864970b7e715f915cb935fb472ff339ff72cdfb
Karthik Selvam <karthik@moo.com>
2018-06-27 15:37:03 +0100

    add babel transform-runtime and es2017 to enable aysnc-await

---
+++ webpack-prod.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: {
+     app: path.join(__dirname, 'src/index.js')
+    },
+    devtool: 'inline-source-map',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.js',
+        publicPath: '/',
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: ['babel-loader']
+        }]
+    },
+};
